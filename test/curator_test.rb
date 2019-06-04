9ca9071c353c626355ce1124a11702dc2e3185b5
Jori Peterson <joripeterson@gmail.com>
2019-06-04 10:27:04 -0600

    Add passing add_photo method

--- test/curator_test.rb
+++ test/curator_test.rb
@@ -11,6 +11,8 @@ class CuratorTest < Minitest::Test
     @curator = Curator.new
     @photo_1 = ({id: "1", name: "Rue Mouffetard, Paris (Boy with Bottles)", artist_id: "1", year: "1954"})
     @photo_2 = ({id: "2", name: "Moonrise, Hernandez", artist_id: "2", year: "1941"})
+    @artist_1 = ({id: "1", name: "Henri Cartier-Bresson", born: "1908", died:"2004", country: "France"})
+    @artist_2 = ({id: "2", name: "Ansel Adams", born: "1902", died: "1984",    country: "United States"}) 
   end
 
   def test_it_exists
@@ -23,8 +25,12 @@ class CuratorTest < Minitest::Test
   end
 
   def test_it_can_add_photographs
-    @curator.add_photograph(@photo_1)
-    @curator.add_photograph(@photo_2)
-    assert_equal [@photo_1, @photo_2], @curator.photographs
+    photo_1 = @curator.add_photograph(@photo_1).last
+    photo_2 = @curator.add_photograph(@photo_2).last
+    assert_equal [photo_1, photo_2], @curator.photographs
+
+    assert_equal photo_1, @curator.photographs.first
+
+    assert_equal "Rue Mouffetard, Paris (Boy with Bottles)", @curator.photographs.first.name
   end
 end
