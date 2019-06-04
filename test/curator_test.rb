ed96e6066da98b13393911a66fdcb8035baba086
Jori Peterson <joripeterson@gmail.com>
2019-06-04 11:03:59 -0600

    Add passing test for photos by artist. Add setup for multiple photos

--- test/curator_test.rb
+++ test/curator_test.rb
@@ -11,8 +11,11 @@ class CuratorTest < Minitest::Test
     @curator = Curator.new
     @photo_1 = ({id: "1", name: "Rue Mouffetard, Paris (Boy with Bottles)", artist_id: "1", year: "1954"})
     @photo_2 = ({id: "2", name: "Moonrise, Hernandez", artist_id: "2", year: "1941"})
+    @photo_3 = ({id: "3", name: "Identical Twins, Roselle, New Jersey", artist_id: "3", year: "1967"})
+    @photo_4 = ({id: "4", name: "Child with Toy Hand Grenade in Central Park", artist_id: "3", year: "1962"})
     @artist_1 = ({id: "1", name: "Henri Cartier-Bresson", born: "1908", died:"2004", country: "France"})
     @artist_2 = ({id: "2", name: "Ansel Adams", born: "1902", died: "1984",    country: "United States"})
+    @artist_3 = ({id: "3", name: "Diane Arbus", born: "1923", died: "1971", country: "United States"})
   end
 
   def test_it_exists
@@ -53,4 +56,31 @@ class CuratorTest < Minitest::Test
     photo_2 = @curator.add_photograph(@photo_2).last
     @curator.find_photograph_by_id("2")
   end
+
+  def test_it_can_find_photographs_by_artist
+    photo_1 = @curator.add_photograph(@photo_1).last
+    photo_2 = @curator.add_photograph(@photo_2).last
+    photo_3 = @curator.add_photograph(@photo_3).last
+    photo_4 = @curator.add_photograph(@photo_4).last
+    artist_1 = @curator.add_artist(@artist_1).last
+    artist_2 = @curator.add_artist(@artist_2).last
+    artist_3 = @curator.add_artist(@artist_3).last
+
+    diane_arbus = @curator.find_artist_by_id("3")
+
+    assert_equal [], @curator.find_photographs_by_artist(diane_arbus)
+  end
+
+  def test_it_can_find_artists_with_multiple_photos
+    photo_1 = @curator.add_photograph(@photo_1).last
+    photo_2 = @curator.add_photograph(@photo_2).last
+    photo_3 = @curator.add_photograph(@photo_3).last
+    photo_4 = @curator.add_photograph(@photo_4).last
+    artist_1 = @curator.add_artist(@artist_1).last
+    artist_2 = @curator.add_artist(@artist_2).last
+    artist_3 = @curator.add_artist(@artist_3).last
+
+    assert_equal artist_3, @curator.artists_with_multiple_photographs
+  end
+
 end
